5d6b3da99c40198158252fdb12ce87ec19876360
angry77cat <angry77cat@users.noreply.github.com>
2018-05-12 00:25:19 +0800

    add wget

--- hw2/hw2_1/hw2_seq2seq.sh
+++ hw2/hw2_1/hw2_seq2seq.sh
@@ -1,1 +1,3 @@
-python model_seq2seq.py $1 $2
+wget -P stored_model https://www.dropbox.com/s/ejxj9qf9r0zwwq4/decoder
+wget -P stored_model https://www.dropbox.com/s/o6nh33ucbo90ly4/encoder
+python3 model_seq2seq.py $1 $2

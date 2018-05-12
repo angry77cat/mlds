3aa10fe77da067c452730783ff5fa0e8c1390678
angry77cat <hhggkkoo@hotmail.com>
2018-05-12 19:55:24 +0800

    update

--- hw2/hw2_1/hw2_seq2seq.sh
+++ hw2/hw2_1/hw2_seq2seq.sh
@@ -1,5 +1,1 @@
-wget -P stored_model https://www.dropbox.com/s/bbgyvwxozu3wruj/decoder
-wget -P stored_model https://www.dropbox.com/s/ra866ly7acl2fcl/encoder
-wget -P stored_model https://www.dropbox.com/s/w5vw8hyk3ujr6r8/vocab.txt
-wget -P stored_model https://www.dropbox.com/s/al8nw81bsk1cf2p/word2vec.128d
 python3 eval_main.py $1 $2

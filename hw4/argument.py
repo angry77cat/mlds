f2f76ed25b40790631fe25c5123be021efc8c5e2
ToastC <mrtoastcheng@gmail.com>
2018-06-19 22:50:36 +0800

    4-1

---
+++ hw4/argument.py
@@ -0,0 +1,19 @@
+def add_arguments(parser):
+    '''
+    Add your arguments here if needed. The TAs will run test.py to load
+    your default arguments.
+
+    For example:
+        parser.add_argument('--batch_size', type=int, default=32, help='batch size for training')
+        parser.add_argument('--learning_rate', type=float, default=0.01, help='learning rate for training')
+    '''
+    
+    parser.add_argument('-b', '--batch_size', type=int, default=5, help='batch size for training')
+    parser.add_argument('-e', '--episode', type=int, default=6000, help='episode')
+    parser.add_argument('-g', '--gamma', type=float, default=0.99, help='gamma')
+    parser.add_argument('-l', '--learning_rate', type=float, default=1e-4, help='learning rate')
+    parser.add_argument('-p', '--pretrain', action='store_true', help='load pretrain model')
+    parser.add_argument('-r', '--render', action='store_true', help='display game')
+    parser.add_argument('--max_buffer', type=int, default=10000)
+
+    return parser
